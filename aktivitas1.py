90983a9ca71b232d9168635f71eb661921ba4268
muhamadbahau <muhamadbahau@gmail.com>
2021-02-26 21:27:28 +0700

    Muhamad Baha'udin_1903380_aktivitas1

---
+++ aktivitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("masukan nama anda: ")
+umur=19
+
+#OUTPUT
+print("")
+print("==== INPUTDAN OUT PADA BAHASA PEMROGRAMAN PHYTHON====")
+print ("nama anda: ", nama)
+print("umur anda: ", umur)
+print("")
